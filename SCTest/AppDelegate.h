e268b08c9075a32ed0fd99f1218b2d9334c306ee
Brian Michel <brian.michel@gmail.com>
2013-02-27 12:35:45 -0500

    Initial Commit

---
+++ SCTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SCTest
+//
+//  Created by Brian Michel on 2/27/13.
+//  Copyright (c) 2013 Foureyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SCTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SCTest
+//
+//  Created by Brian Michel on 2/27/13.
+//  Copyright (c) 2013 Foureyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
