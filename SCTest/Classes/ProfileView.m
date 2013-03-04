dd5d4632bbf6df516549e8b465351fbcaafef1d6
Brian Michel <brian.michel@gmail.com>
2013-03-04 15:25:47 -0500

    cleanup

--- SCTest/Classes/ProfileView.m
+++ SCTest/Classes/ProfileView.m
@@ -92,13 +92,5 @@ const CGFloat kProfileViewAvatarHW = 100.0;
 - (NSDictionary *)userInformationDictionary {
   return _userInformationDictionary;
 }
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
